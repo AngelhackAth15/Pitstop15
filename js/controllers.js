debdabde71762ad291af593be53812cf11bcdfd2
nliapis <nliapis1983@gmail.com>
2015-06-06 13:24:24 +0300

    add http post method

--- js/controllers.js
+++ js/controllers.js
@@ -3,6 +3,23 @@ angular.module('App.controllers', []).
 controller('MainController', ['$scope', '$http', function($scope, $http) {
     'use strict';
 
-    console.log('Hello');
+    $http({
+        url: '',
+        method: 'POST',
+        dataType: 'JSON',
+        data: {
+            tag: addTag
+        }
+    }).success(function(data, status, headers, config) {
+        if (data.code == 200) {
+
+        }else if (data.code == 400) {
+
+        }
+    }).error(function(data, status, headers, config) {
+
+    }).finally(function(){
+
+    });
 
 }]);
