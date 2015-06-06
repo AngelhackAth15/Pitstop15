1dff524eef734be84bc405bee3c1464cdb23c71d
nliapis <nliapis1983@gmail.com>
2015-06-06 14:42:33 +0300

    set all call to end points

--- js/controllers.js
+++ js/controllers.js
@@ -5,12 +5,10 @@ controller('MainController', ['$scope', '$http', function($scope, $http) {
 
     var setValues = function(){
     	$http({
-	        url: 'my-service.com/api/v1/set/keys' + $scope.setKey,
+	        url: 'my-service.com/api/v1/set/keys/' + $scope.setInputValues.key,
 	        method: 'POST',
 	        dataType: 'JSON',
-	        data: {
-	            value: $scope.setValue
-	        }
+	        data: $scope.setInputValues.value
 	    }).success(function(data, status, headers, config) {
 	        // if (data.code == 200) {
 
@@ -24,9 +22,13 @@ controller('MainController', ['$scope', '$http', function($scope, $http) {
 	    });
     };
 
+    $scope.setKeyValue = function(){
+    	setValues();
+    };
+
     var getValues = function(){
     	$http({
-	        url: 'my-service.com/api/v1/get/keys' + $scope.getKey,
+	        url: 'my-service.com/api/v1/get/keys/' + $scope.getInputValues.key,
 	        method: 'GET',
 	        dataType: 'JSON'
 	    }).success(function(data, status, headers, config) {
@@ -42,13 +44,40 @@ controller('MainController', ['$scope', '$http', function($scope, $http) {
 	    });
     };
 
+    $scope.getKeyValue = function(){
+    	getValues();
+    };
+
     var sendMessages = function(){
     	$http({
-	        url: 'my-service.com/api/v1/send-message/' + $scope.setTopic,
+	        url: 'my-service.com/api/v1/send-message/topics/' + $scope.setInputMessages.topic,
+	        method: 'POST',
+	        dataType: 'JSON',
+	        data: $scope.setInputMessages.message
+	    }).success(function(data, status, headers, config) {
+	        // if (data.code == 200) {
+
+	        // }else if (data.code == 400) {
+
+	        // }
+	    }).error(function(data, status, headers, config) {
+
+	    }).finally(function(){
+
+	    });
+    };
+    
+    $scope.setTopicMessage = function(){
+    	sendMessages();
+    };
+
+    var getMessages = function(){
+    	$http({
+	        url: 'my-service.com/api/v1/get-message/topics/' + $scope.getInputMessages.topic +'/'+ $scope.getInputMessages.message,
 	        method: 'POST',
 	        dataType: 'JSON',
 	        data: {
-	            message: $scope.setMessage
+	            message: $scope.getInputMessages.message
 	        }
 	    }).success(function(data, status, headers, config) {
 	        // if (data.code == 200) {
@@ -63,25 +92,8 @@ controller('MainController', ['$scope', '$http', function($scope, $http) {
 	    });
     };
 
-    var getMessages = function(){
-    	$http({
-	        url: 'my-service.com/api/v1/get-message/' + $scope.getTopic +'/'+ $scope.getNumber,
-	        method: 'POST',
-	        dataType: 'JSON',
-	        data: {
-	            message: $scope.setMessage
-	        }
-	    }).success(function(data, status, headers, config) {
-	        // if (data.code == 200) {
-
-	        // }else if (data.code == 400) {
-
-	        // }
-	    }).error(function(data, status, headers, config) {
-
-	    }).finally(function(){
-
-	    });
+    $scope.getTopicMessage = function(){
+    	getMessages();
     };
 
 
