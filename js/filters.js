2d17e89203c4f63f7470e003d9ea197ca223b0a0
nliapis <nliapis1983@gmail.com>
2015-06-06 13:15:10 +0300

    set up angular

--- js/filters.js
+++ js/filters.js
@@ -1,13 +1,1 @@
-angular.module('App.filters', []).
-
-filter('htmlToPlaintext', function() {
-    return function(text) {
-        return String(text).replace(/<[^>]+>/gm, '');
-    };
-}).
-
-filter("formatHtml", ['$sce', function($sce) {
-    return function(input) {
-        return $sce.trustAsHtml(input);
-    };
-}]);
+angular.module('App.filters', []);
